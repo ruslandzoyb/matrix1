ca6c3f505e1c2f71a21786f6474e4fb309e14f47
rusya <dnepr.ukrainec@gmail.com>
2019-09-27 23:00:24 +0300

    new

--- Matrix/Matrix.cs
+++ Matrix/Matrix.cs
@@ -6,8 +6,59 @@ using System.Threading.Tasks;
 
 namespace Matrix
 {
-    class Matrix
+    class Matrix_model
     {
+        private double[,] matrix;
+        private ushort row, column;
+
+        public Matrix_model(double[,] matrix,ushort i,ushort j)
+        {
+            if (i*j==matrix.Length )
+            {
+                try
+                {
+                    this.matrix = matrix;
+                    Dimension(i, j);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+        public Matrix_model(ushort i,ushort j)
+        {
+            Dimension(i, j);
+        }
+        private void Dimension(ushort i,ushort j)
+        {
+            row = i;
+            column = j;
+        }
+
+        public double[,] Matrix
+        {
+            get
+            {
+                return this.matrix;
+            }
+        }
+
+        public double this[ushort i,ushort j]
+        {
+            get
+            {
+                if (i<=row && j<=column)
+                {
+                    return this.matrix[i, j];
+                }
+                else
+                {
+                    throw new Exception("Выход за границы массива ");
+                }
+            }
+        }
 
     }
 }
