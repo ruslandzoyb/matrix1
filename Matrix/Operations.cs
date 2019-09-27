a69b0d7a596669340c6f07867e2c4b40f33b155d
rusya <dnepr.ukrainec@gmail.com>
2019-09-27 23:43:38 +0300

    Matrix

---
+++ Matrix/Operations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Operations
+    {
+       public static Matrix Addition(Matrix first,Matrix second)
+        {
+           
+            if (first.Row==second.Row && first.Column==second.Column)
+            {
+                Matrix matrix = new Matrix(first.Row, first.Column);
+                for (ushort i = 0; i < first.Row; i++)
+                {
+                    for (ushort j = 0; j < first.Column; j++)
+                    {
+                        matrix[i, j] = first[i, j] + second[i, j];
+                    }
+                }
+                return matrix;
+            }
+            else
+            {
+                throw new Exception("Не равные матрицы ");
+            }
+        }
+
+
+    }
+}
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -11,7 +11,13 @@ namespace Matrix
         static void Main(string[] args)
         {
             Matrix matrix = new Matrix(new[,] { { 1.0, 2, 3 }, { 4, 8, 9 } }, 2, 3);
+            Matrix matrix2 = new Matrix(new[,] { { 1.0, 1, 7 }, { 0, 4, 2 } }, 2, 3);
             matrix.Show_Matrix();
+            Console.WriteLine();
+            matrix2.Show_Matrix();
+            Console.WriteLine();
+            Matrix mat = Operations.Addition(matrix, matrix2);
+            mat.Show_Matrix();
 
             Console.ReadKey();
         }
